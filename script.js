d7e1721594a0ad230f346f93002b03cc5936cce4
avship <alexandershipilo@yandex.ru>
2022-06-01 15:19:06 +0300

    Домашка готова

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
+
+alert("Моя первая домашка на курсе Glo Academy Полный Фарш");
+console.log("Какой-то текст в консоли");
