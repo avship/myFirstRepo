3b337e12256b40d977fb39d48415781f556d3cfd
avship <alexandershipilo@yandex.ru>
2022-06-01 19:11:52 +0300

    Задание урока 2. Базовое

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const title = 1;
-const screens = 2;
-const screenPrice = 3;
-const rollback = 4;
-const fullPrice = 5;
-const adaptive = 6;
+const title = "Проект lesson 2";
+const screens = '("Простые, Сложные, Интерактивные")';
+const screenPrice = 777;
+const rollback = Math.round(Math.random() * 100 + 0.5);
+const fullPrice = 240000;
+const adaptive = true;
 
-alert("Моя первая домашка на курсе Glo Academy Полный Фарш");
-console.log("Задание 1 готово");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(`Длина переменной screens: ${screens.length}`);
+console.log(
+  `Стоимость верстки экранов (${screenPrice}) рублей/ долларов/гривен/юани`
+);
+console.log(
+  `Стоимость разработки сайта (${fullPrice}) рублей/ долларов/гривен/юани`
+);
+console.log(screens.toLowerCase().split(","));
+console.log(
+  `Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`
+);
