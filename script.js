d730fed2fe5ae4850cd1a5fce9f8e489d56961b9
avship <alexandershipilo@yandex.ru>
2022-06-01 23:20:25 +0300

    Исправленный баг с подсчётом числа. Использую floor, а не round

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let num = 266219;
-
 let productDigits = 1;
+let power3 = null;
 while (num !== 0) {
   const lastDigit = num % 10;
   //console.log(lastDigit);
   productDigits *= lastDigit;
-  num = Math.round(num / 10);
+  num = Math.floor(num / 10);
 }
-let power3 = productDigits ** 3;
+power3 = productDigits ** 3;
 console.log(String(power3).slice(0, 2));
