ae8d2d0b8a65dc7d41009d46970c1876f59a3789
avship <alexandershipilo@yandex.ru>
2022-06-02 17:38:05 +0300

    Сложное задание к уроку 3 готово"
    э

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-"use strict";
-const title = prompt("Как называется ваш проект?");
-const screens = prompt("Какие типы экранов нужно разработать?");
-const screenPrice = +prompt("Сколько будет стоить данная работа?");
-const rollback = Math.round(Math.random() * 100 + 0.5);
-//const fullPrice = 240000;
-const adaptive = confirm("Нужен ли адаптив на сайте?");
+//Выбираем значение переменной lang меняем true и false
+const lang = true ? "ru" : "en";
 
-const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = prompt("Сколько это будет стоить?");
-const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = prompt("Сколько это будет стоить?");
-
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-const servicePercentPrice = Math.ceil(fullPrice - rollback);
-console.log(servicePercentPrice);
-
-switch (true) {
-  case fullPrice >= 30000:
-    console.log("Даем скидку в 10%");
+if (lang == "ru") {
+  console.log(
+    "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+  );
+} else {
+  console.log("Monday, tuesday, wednesday, thursday, friday, saturday, sunday");
+}
+console.log("");
+switch (lang) {
+  case "en":
+    console.log(
+      "Monday, tuesday, wednesday, thursday, friday, saturday, sunday"
+    );
     break;
-  case fullPrice >= 15000:
-    console.log("Даем скидку в 5%");
-    break;
-  case fullPrice >= 0:
-    console.log("Скидка не предусмотрена");
+  case "ru":
+    console.log(
+      "Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"
+    );
     break;
   default:
-    console.log("Что-то пошло не так");
+    console.log("Ошибка");
+    break;
 }
+const days = [
+  ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"],
+  ["Monday, tuesday, wednesday, thursday, friday, saturday, sunday"],
+];
+
+// Вывод ныжной строчки
+
+console.log(days[lang === "ru" ? 0 : 1]);
