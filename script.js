c28704754e4b2f9e7dc147038ba23d217da2ddb6
avship <alexandershipilo@yandex.ru>
2022-06-03 12:42:00 +0300

    Хард второго уровня готов

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 "use strict";
 
-const isNumber = function (num) {
-  num = String(num);
-  return (
-    (num.match(/^\d+(\.\d+)?$/) && !isNaN(parseFloat(num)) && isFinite(num)) ===
-    true
-  );
-};
+//часть 1
+const arr = ["123", "567", "9873", "6013", "456", "222", "777"];
+arr.forEach((element) => {
+  if (element.match(/^[24]/)) {
+    console.log(element);
+  }
+});
 
-console.log(isNumber(null));
-console.log(isNumber("      2345         "));
-console.log(isNumber("2345"));
-console.log(isNumber("2345.56"));
+//часть 2 простые числа
+function isPrime(n) {
+  let j = 1;
+  for (let divisor = 2; divisor < Math.ceil(n / 2); divisor++) {
+    if (n % divisor === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+for (let n = 2; n <= 100; n++) {
+  if (isPrime(n)) {
+    console.log(`${n} Делители этого числа: 1 и ${n}`);
+  }
+}
